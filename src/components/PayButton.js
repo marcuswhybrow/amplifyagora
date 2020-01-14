f322d443564cd3c25d9f3e98daeb1a240973cc5b
Marcus Whybrow <m@rcus.fit>
2020-01-14 22:36:31 +0000

    done lesson 46

--- src/components/PayButton.js
+++ src/components/PayButton.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { API } from "aws-amplify";
 import StripeCheckout from "react-stripe-checkout";
 // import { Notification, Message } from "element-react";
 
@@ -8,8 +9,22 @@ const stripeConfig = {
 }
 
 const PayButton = ({ product, user }) => {
+  const handleCharge = async token => {
+    try {
+      const result = await API.post("orderlambda", "/charge", {
+        body: {
+          token
+        }
+      });
+      console.log({ result });
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   return (
     <StripeCheckout
+      token={handleCharge}
       email={user.attributes.email}
       name={product.description}
       amount={product.price}
