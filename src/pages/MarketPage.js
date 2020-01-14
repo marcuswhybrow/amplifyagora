e53cae29b0ff7d83bb333afc17c03885ee63fa44
Marcus Whybrow <m@rcus.fit>
2020-01-14 21:03:04 +0000

    done lesson 41

--- src/pages/MarketPage.js
+++ src/pages/MarketPage.js
@@ -1,10 +1,34 @@
 import React from "react";
 import { API, graphqlOperation } from "aws-amplify";
-import { getMarket } from "../graphql/queries";
 import { Loading, Tabs, Icon } from "element-react";
 import { Link } from "react-router-dom";
 import NewProduct from "../components/NewProduct";
 import Product from "../components/Product";
+
+const getMarket = `query GetMarket($id: ID!) {
+  getMarket(id: $id) {
+    id
+    name
+    products {
+      items {
+        id
+        description
+        price
+        shipped
+        owner
+        file {
+          key
+        }
+        createdAt
+      }
+      nextToken
+    }
+    tags
+    owner
+    createdAt
+  }
+}
+`;
 
 class MarketPage extends React.Component {
   state = {
@@ -84,11 +108,11 @@ class MarketPage extends React.Component {
             }
             name="2"
           >
-            {/* <div className="product-list">
+            <div className="product-list">
               {market.products.items.map(product => (
-                <Product product={product} />
+                <Product key={product.id} product={product} />
               ))}
-            </div> */}
+            </div>
           </Tabs.Pane>
         </Tabs>
       </>
