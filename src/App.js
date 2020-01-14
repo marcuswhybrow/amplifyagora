3394dfc804a82b68eab995db353a6ef152e4346a
Marcus Whybrow <m@rcus.fit>
2020-01-14 16:43:15 +0000

    done lesson 27

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Auth, Hub } from 'aws-amplify';
 import { Authenticator, AmplifyTheme } from 'aws-amplify-react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ProfilePage from './pages/ProfilePage';
+import MarketPage from './pages/MarketPage';
+import Navbar from './components/Navbar';
 import "./App.css";
-import { isThisISOWeek } from "date-fns";
 
 class App extends React.Component {
   state = {
     user: null
-  };
+  }
 
   componentDidMount() {
     this.getUserData();
@@ -37,12 +41,36 @@ class App extends React.Component {
     }
   }
 
+  handleSignout = async () => {
+    try {
+      await Auth.signOut()
+    } catch (err) {
+      console.error('Error signing out user', err);
+    }
+  }
+
   render() {
     const { user } = this.state;
 
     return !user ? (
       <Authenticator theme={theme} />
-    ) : <div>App</div>;
+    ) : (
+      <Router>
+        <>
+          {/* Navigation */}
+          <Navbar user={user} handleSignout={this.handleSignout} />
+
+          {/* Routes */}
+          <div className="app-container">
+            <Route exact path="/" component={HomePage} />
+            <Route path="/profile" component={ProfilePage} />
+            <Route path="/markets/:marketId" component={
+              ({ match }) => <MarketPage marketId={match.params.marketId} />
+            } />
+          </div>
+        </>
+      </Router>
+    );
   }
 }
 
