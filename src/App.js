078a33edb472825692610a31f0fa8ab2da1e521d
Marcus Whybrow <m@rcus.fit>
2020-01-14 16:20:35 +0000

    done lesson 26

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
 import React from "react";
-import { withAuthenticator, AmplifyTheme } from 'aws-amplify-react';
+import { Auth, Hub } from 'aws-amplify';
+import { Authenticator, AmplifyTheme } from 'aws-amplify-react';
 import "./App.css";
+import { isThisISOWeek } from "date-fns";
 
 class App extends React.Component {
-  state = {};
+  state = {
+    user: null
+  };
+
+  componentDidMount() {
+    this.getUserData();
+    Hub.listen('auth', this, 'onHubCapsule');
+  }
+
+  getUserData = async () => {
+    const user = await Auth.currentAuthenticatedUser();
+    user ? this.setState({ user }) : this.setState({ user: null });
+  }
+
+  onHubCapsule = capsule => {
+    switch (capsule.payload.event) {
+      case "signIn":
+        console.log("signed in");
+        this.getUserData();
+        break;
+      case "signUp":
+        console.log("signed up");
+        break;
+      case "signOut":
+        console.log("signed out");
+        this.setState({ user: null });
+        break;
+      default:
+        return;
+    }
+  }
 
   render() {
-    return <div>App</div>;
+    const { user } = this.state;
+
+    return !user ? (
+      <Authenticator theme={theme} />
+    ) : <div>App</div>;
   }
 }
 
@@ -30,4 +66,5 @@ const theme = {
   }
 };
 
-export default withAuthenticator(App, true, [], null, theme);
+// export default withAuthenticator(App, true, [], null, theme);
+export default App;
